33f592d314d72fb5a1bcb914e2d685ea21dff3ae
emilykanter <ekanter@sas.upenn.edu>
2021-12-13 17:13:06 -0500

    final

--- code/6-misclassification-error.R
+++ code/6-misclassification-error.R
@@ -6,29 +6,37 @@ misclassification_decision = read_csv("results/misclassification_decision.csv")
 misclassification_rf = read_csv("results/misclassification_rf.csv")
 
 # renaming column names 
- misclassification_regression = misclassification_regression %>% 
-  rename(value = `mean(mentally_unhealthy != predicted_mental_health)`)
- misclassification_ridge = misclassification_ridge %>% 
-   rename(value = `mean(mentally_unhealthy != predicted_mental_health)`)
- misclassification_lasso = misclassification_lasso %>% 
-   rename(value = `mean(mentally_unhealthy != predicted_mental_health)`)
+misclassification_regression = misclassification_regression %>% 
+  rename(value = `mean(mentally_unhealthy != predicted_mental_health)`) %>%
+  mutate(value = as.numeric(value))
+ 
+misclassification_ridge = misclassification_ridge %>% 
+   rename(value = `mean(mentally_unhealthy != predicted_mental_health)`) %>%
+   mutate(value = as.numeric(value))
+ 
+misclassification_lasso = misclassification_lasso %>% 
+   rename(value = `mean(mentally_unhealthy != predicted_mental_health)`) %>%
+   mutate(value = as.numeric(value))
+
+misclassification_decision = misclassification_decision %>%
+   mutate(value = as.numeric(value))
+ 
+misclassification_rf = misclassification_rf %>%
+   mutate(value = as.numeric(value))
 
 # creating table of misclassification errors 
 Model = c("Regression", "Ridge", "Lasso", "Decision", "Random Forest")
 
 misclassification =  
-   as_tibble(cbind(Model, rbind(misclassification_regression$value, 
-      misclassification_ridge$value, 
-       misclassification_lasso$value, 
-       misclassification_decision$value, 
-       misclassification_rf$value))) %>% 
-  rename(`Misclassification Error` = V2) %>%
+   as_tibble(cbind(Model, rbind(misclassification_regression, 
+      misclassification_ridge, 
+      misclassification_lasso, 
+      misclassification_decision, 
+      misclassification_rf))) %>% 
   kable(format = "latex",
         booktabs = TRUE, 
-        digits = c(1, 4),
+        digits = 4,
         col.names = c("Feature", "Coefficient"),
         caption = "Misclassification errors for each model.") %>%
   save_kable("misclassification-error.pdf")
  
-
-
