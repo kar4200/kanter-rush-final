856547546c33bce5f6dd1e3a2cc5b425c24be43e
emilykanter <ekanter@sas.upenn.edu>
2021-12-18 13:59:26 -0500

    final

--- code/4-regression-modeling.R
+++ code/4-regression-modeling.R
@@ -12,93 +12,40 @@ mental_health_train = read_csv("data/clean/mental_health_train.csv")
 mental_health_test = read_csv("data/clean/mental_health_test.csv")
 
 # running a logistic regression
-set.seed(1)
-glm_fit = glm(mentally_unhealthy ~ . -mentally_unhealthy_days
-                                     -physically_unhealthy_days, 
-              family = "binomial",
+lm_fit = lm(mentally_unhealthy_days ~ . -physically_unhealthy_days, 
               data = mental_health_train)
 
-summary(glm_fit)
+summary(lm_fit)
 
 ## extracting elements of the fit
-coef(glm_fit)
+coef(lm_fit)
+
+# r-squared value
+summary(lm_fit)$r.squared # 0.8274298
+
+# check for distribution and homoscedasticity of the residuals to make sure we do not have to log data  
+hist(lm_fit$residuals)
+plot(lm_fit$residuals)
+# add to appendix - but everything is good 
 
 # extracting the fitted probabilities 
-fitted_probabilities_test = predict(glm_fit, 
+fitted_probabilities_test = predict(lm_fit, 
                                newdata = mental_health_test,
                                type = "response") 
 
-fitted_probabilities_train = predict(glm_fit, 
+fitted_probabilities_train = predict(lm_fit, 
                                newdata = mental_health_train,
                                type = "response")   
 
-# make predictions 
-predictions_test = as.numeric(fitted_probabilities_test > 0.5)
+# evaluating the classifier - calculate rmse for train and test
+RMSE_test_lm = sqrt(mean((fitted_probabilities_test - mental_health_test$mentally_unhealthy_days)^2)) %>%
+  data.frame()
 
-predictions_train = as.numeric(fitted_probabilities_train > 0.5)
+RMSE_train_lm = sqrt(mean((fitted_probabilities_train - mental_health_train$mentally_unhealthy_days)^2)) %>%
+  data.frame()
 
-# setting difference probability thresholds: 
-predictions_test_ = as.numeric(fitted_probabilities_test > 0.5)
+write_csv(RMSE_test_lm, 
+          file = "results/rmse_test_lm.csv")
 
-mvalues = seq.int(1, 60, by = 5)
-oob_errors = numeric(length(mvalues))
-ntree = 100
-for(idx in 1:length(mvalues)){
-  m = mvalues[idx]
-  rf_fit_test = randomForest(factor(mentally_unhealthy) ~ . -mentally_unhealthy_days -physically_unhealthy_days, 
-                             mtry = m, data = mental_health_train)
-  oob_errors[idx] = rf_fit_test$err.rate[,"OOB"][ntree]
-}
-
-# evaluating the classifier 
-mental_health_test = mental_health_test %>% 
-  mutate(predicted_mental_health = predictions_test)
-
-mental_health_train = mental_health_train %>% 
-  mutate(predicted_mental_health = predictions_train)
-
-# calculate misclassification rate
-misclassification_test_regression = mental_health_test %>% 
-  summarise(mean(mentally_unhealthy != predicted_mental_health))
-
-misclassification_train_regression = mental_health_train %>% 
-  summarise(mean(mentally_unhealthy != predicted_mental_health))
-
-write_csv(misclassification_test_regression, 
-          file = "results/misclassification_test_regression.csv")
-
-write_csv(misclassification_train_regression, 
-          file = "results/misclassification_train_regression.csv")
-
-# confusion matrix 
-mental_health_test %>% 
-  select(mentally_unhealthy, predicted_mental_health) %>%
-  table()
-
-fpr = 20 / (370 + 20)
-fnr = 19 / (19 + 65)
-
-# ROC curve
-roc_data = roc(mental_health_test %>% 
-                 pull(mentally_unhealthy), 
-               fitted_probabilities_test) 
-
-p_roc = tibble(FPR = 1-roc_data$specificities,
-       TPR = roc_data$sensitivities) %>%
-  ggplot(aes(x = FPR, y = TPR)) + 
-  geom_line() + 
-  xlab("False Positive Rate (FPR)") +
-  ylab("True Positive Rate (TPR)") +
-  geom_abline(slope = 1, linetype = "dashed", color = "red") +
-  geom_point(x = fpr, y = 1-fnr, colour = "red") +
-  theme_bw()
-
-ggsave(filename = "results/roc-table.png", 
-       plot = p_roc, 
-       device = "png", 
-       width = 5, 
-       height = 5)
-
-# print the AUC
-roc_data$auc
-
+write_csv(RMSE_train_lm, 
+          file = "results/rmse_train.lm.csv")
