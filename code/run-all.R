f29da11f8bfcf63039e0e8508b0012685d09e944
emilykanter <ekanter@sas.upenn.edu>
2021-12-18 20:58:15 -0500

    final

--- code/run-all.R
+++ code/run-all.R
@@ -1,8 +1,17 @@
 # run all steps of the analysis pipeline
 source("code/0-download.R")
 source("code/1-cleaning.R")
-source("code/2-exploration.R")
+source("code/2-exploration-response.R")
+
+# train-test split before using mental health train and test in other files
 source("code/3-train-test-split.R")
+
+source("code/2.5-exploration-features.R")
+source("code/2.5-exploration-features/response.R")
 source("code/4-regression-modeling.R")
+source("code/4.5-elasticnet-modeling.R")
+source("code/4.5-lasso-modeling.R")
+source("code/4.5-ridge-modeling.R")
 source("code/5-tree-modeling.R")
+source("code/5-boosted-modeling.R")
 source("code/6-model-evaluation.R")
