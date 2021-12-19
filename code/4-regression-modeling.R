79e9e812a9c8bdefee078c48e95a9b899f86b287
emilykanter <ekanter@sas.upenn.edu>
2021-12-19 10:45:33 -0500

    final

--- code/4-regression-modeling.R
+++ code/4-regression-modeling.R
@@ -15,35 +15,13 @@ summary(lm_fit)
 
 save(lm_fit, file = "results/lm_fit.Rda")
 
-## extracting elements of the fit
+# extracting elements of the fit
 coef(lm_fit)
 
 # r-squared value
 summary(lm_fit)$r.squared # 0.8274298
 
-# check for distribution and homoscedasticity of the residuals to make sure we do not have to log data  
+# check for distribution and heteroscedasticity of the residuals to make sure we do not have to log data  
 hist(lm_fit$residuals)
 plot(lm_fit$residuals)
-# add to appendix - but everything is good 
 
-# extracting the fitted probabilities 
-predictions_test_lm = predict(lm_fit, 
-                               newdata = mental_health_test) 
-
-predictions_train_lm = predict(lm_fit, 
-                               newdata = mental_health_train)   
-
-# evaluating the classifier - calculate rmse for train and test
-mse_test_lm = mean((predictions_test_lm - 
-                      mental_health_test$mentally_unhealthy_days)^2) %>%
-  as_tibble()
-
-mse_train_lm = mean((predictions_train_lm - 
-                       mental_health_train$mentally_unhealthy_days)^2) %>%
-  as_tibble()
-
-write_csv(mse_test_lm, 
-          file = "results/mse_test_lm.csv")
-
-write_csv(mse_train_lm, 
-          file = "results/mse_train.lm.csv")
